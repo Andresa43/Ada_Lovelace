8e4aa59774ab40e715ad86f35409bbbaca3d06bd
Andresa43 <andresa.ifba@gmail.com>
2023-02-26 07:26:54 -0300

    refactor: insert variables

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
+var nome = document.getElementById("nome-usuario").Value
+var email = document.getElementById("email-usuario").value 
+var telefone = document.getElementById("telefone").value
 
 function validaFormulario() {
-    if (document.getElementById("nome-usuario").Value != "" && document.getElementById("email-usuario").value != "" && document.getElementById("telefone").value != "") {
+    if ( nome != "" && email != "" &&  telefone != "") {
         alert("Protinho! Você receberá as novidades no email")
     } else {
         alert("Por favor, preencha os campos nome e email")
